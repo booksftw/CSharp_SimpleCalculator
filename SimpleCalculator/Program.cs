1eef372a2795ef4404aee0bf5c035b51f8f37bc9
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-02-21 22:02:27 -0800

    complete acceptance criteria

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNumberic(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumberic(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                // In real world we would want to log this message
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
